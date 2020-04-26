40a51fc1820fa145e8d5feeadb1d82aa5b8b7841
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-04-26 01:14:12 -0400

    Crear

---
+++ laboratorio5-AntoniaCarrizo/laboratorio5-AntoniaCarrizo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace laboratorio5AntoniaCarrizo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
